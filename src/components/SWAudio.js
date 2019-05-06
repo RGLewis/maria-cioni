e2db0c2cea1d5bf3ddb0fff9ebbad63555cbcdf3
Rafaela <rafi.lewis@me.com>
2019-05-06 16:16:24 -0400

    sw section done for now

--- src/components/SWAudio.js
+++ src/components/SWAudio.js
@@ -1,4 +1,6 @@
 import React from "react";
+import SWAudioCover from "../assets/swAudiobook.jpg";
+import SWAudioSample from "../assets/sw-audiobook-sample.mp3";
 
 const SWAudio = (props) => {
     const audioToggleBar = props.audioIsShown ? "hide-show-bar vertical vertical-to-horizontal" : "hide-show-bar vertical";
@@ -23,10 +25,34 @@ const SWAudio = (props) => {
             </div>
 
             {props.audioIsShown &&
-                <div className="audiobook">
-                    <p>What a luxury, having someone read to you! For me, this is the allure of an audiobook format.  Being read to usually starts during childhood, with a bedtime story.</p>
-                    <p>When my daughter was small, she would often ask for a bedtime story about my childhood, growing up in La Villa restaurant.  The seed was planted - Spaghetti Western needed to be an audiobook.  And here it is, read to you by my daughter, herself.</p>
+                <div className="audiobook-show-container">
+                    {/* Audiobook flex container starts */}
+                    <div className="audiobook-flex-container">
+
+                        <div className="audio-cover-container">
+                            <img src={SWAudioCover} alt="The Audiobook cover of 'Spaghetti Western' -- a black and white photo of a young Maria, with her father, Gene." />
+                        </div>
+
+                        <div className="audio-written-container">
+                            <p className="larger-para">A message from Maria</p>
+
+                            <p>What a luxury, having someone read to you! For me, this is the allure of an audiobook format.  Being read to usually starts during childhood, with a bedtime story.</p>
+
+                            <p>When my daughter was small, she would often ask for a bedtime story about my childhood, growing up in La Villa restaurant.  The seed was planted - Spaghetti Western needed to be an audiobook.  And here it is, read to you by my daughter, herself.</p>
+                        </div>
+                    {/* Audiobook flex container ends */}
+                    </div>
+                
+                    <div className="audio-sample-container">
+                        <p className="larger-para">Listen to a sample</p>
+
+                        <audio className="audiobook-sample" src={SWAudioSample} controls>
+                            Your browser does not support the audio element.
+                        </audio>
+                    </div>
                 </div>
+
+                
             }
     
         </section>
--- src/components/SWGet.js
+++ src/components/SWGet.js
@@ -2,13 +2,17 @@ import React from "react";
 
 const SWGet = () => (
     <div className="get-spaghetti-western">
-        <p className="experience-sw">Experience Spaghetti Western</p>
+        <p className="larger-para">Experience Spaghetti Western</p>
         
         <a href="https://www.amazon.ca/Spaghetti-Western-Father-Brought-Italian/dp/B07D9T3D9Z/ref=sr_1_1?keywords=spaghetti+western+maria+cioni&qid=1556996196&s=gateway&sr=8-1-spell" className="sw-get-link">Amazon</a>
         
         <span className="slash">/</span>
         
         <a href="https://www.audible.ca/pd/Spaghetti-Western-Audiobook/B07D9T3L5X" className="sw-get-link">Audible</a>
+
+        <span className="slash">/</span>
+
+        <a href="https://itunes.apple.com/ca/audiobook/spaghetti-western-how-my-father-brought-italian-food/id1391039810" className="sw-get-link">iTunes</a>
     </div>
 )
 
